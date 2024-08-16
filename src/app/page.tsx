3ae402a40a12bc150901998985be2bc3f2f97e79
Sebastian Perez <112594443+SebaPerez90@users.noreply.github.com>
2024-08-16 13:25:52 -0300

    post product UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,22 @@ import About from '@/components/About';
 import Footer from '@/components/Footer';
 import Landing from '@/components/Hero/Landing';
 import Header from '@/components/NavBar/Header';
-import ProductCard from '@/components/Products/ProductCard';
-import ProductList from '@/components/Products/ProductList';
+import ProductCarrousel from '@/components/Products/ProductCarrousel';
+// import ProductList from '@/components/Products/ProductList';
 
 const page = () => {
   return (
     <>
-      <ProductList />
-      {/* <Header />
+      <Header />
       <main
         id='main-section'
         className='pt-40 text duration-500'>
         <Landing />
         <About />
+        <ProductCarrousel />
+        {/* <ProductList /> */}
       </main>
-      <Footer /> */}
+      <Footer />
     </>
   );
 };
