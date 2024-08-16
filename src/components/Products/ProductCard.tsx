1a5c89a245ac31c17bfaf62c46b77b9fb0a18b89
Sebastian Perez <112594443+SebaPerez90@users.noreply.github.com>
2024-08-16 09:55:58 -0300

    product card UI

--- src/components/Products/ProductCard.tsx
+++ src/components/Products/ProductCard.tsx
@@ -8,9 +8,8 @@ import { FaFire } from 'react-icons/fa';
 import { useState } from 'react';
 
 const ProductCard = () => {
-  const [favorites, setFavorites] = useState<{ [key: number]: boolean }>({});
-
   const discountPercentage = 0.2;
+  const [favorites, setFavorites] = useState<{ [key: number]: boolean }>({});
 
   const addToFavorite = (index: number) => {
     setFavorites((prevFavorites) => ({
@@ -28,7 +27,7 @@ const ProductCard = () => {
         {fake_data.map((product: IProduct, index) => (
           <div
             key={index}
-            className='relative flex flex-col items-center justify-between py-4  rounded-lg shadow-neutral gap-4 w-[18em] bg-white'>
+            className='relative flex flex-col items-center justify-between py-4 rounded-lg shadow-neutral gap-4 w-[22em] bg-white'>
             <svg
               id='heart-svg'
               xmlns='http://www.w3.org/2000/svg'
@@ -38,7 +37,12 @@ const ProductCard = () => {
               strokeWidth='1'
               strokeLinecap='round'
               strokeLinejoin='round'
-              className='feather feather-heart h-8 w-8 absolute right-3 top-3 cursor-pointer duration-1000'
+              style={
+                favorites[index]
+                  ? { animation: 'growEffect 500ms linear' }
+                  : undefined
+              }
+              className='feather feather-heart h-8 w-8 absolute right-4 top-4 cursor-pointer duration-1000'
               onClick={() => addToFavorite(index)}>
               <path d='M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 1 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z' />
             </svg>
@@ -49,7 +53,7 @@ const ProductCard = () => {
                 height={300}
                 src={product.image}
                 alt={product.title}
-                className='w-full h-full object-contain z-20'
+                className='w-full h-full object-contain z-20 hover:scale-110 duration-200'
               />
             </div>
             <span className='text-base-font-300 flex flex-col gap-2 font-bold text-lg w-[90%] text-center'>
@@ -80,16 +84,19 @@ const ProductCard = () => {
                 product.price
               )}
               {product.rating.rate < 3.5 && (
-                <span className='absolute top-2 left-2 text-[0.7em] flex items-center gap-1 border rounded-md px-3 font-medium bg-[#ffffff1f] [backdrop-filter:blur(5px)]'>
+                <span className='absolute top-2 left-2 text-[0.8em] flex items-center gap-1 border rounded-md px-3 font-medium bg-[#ffffff1f] [backdrop-filter:blur(5px)]'>
                   Oferta
                   <FaFire className='text-red-500' />
                 </span>
               )}
             </span>
-            <button className='btn_secondary flex items-center gap-2 '>
-              Sumar al carro
-              <MdAddShoppingCart className='text-xl' />
-            </button>
+            <div className='flex items-center gap-2'>
+              <button className='btn_secondary py-3 px-6'>Ver más</button>
+              <button className='btn_primary flex items-center gap-2 py-3 px-6'>
+                Sumar al carro
+                <MdAddShoppingCart className='text-2xl' />
+              </button>
+            </div>
           </div>
         ))}
       </div>
